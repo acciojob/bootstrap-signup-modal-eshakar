5b2e15e900c0cc3771e1a97cea1c1f38e5c58e74
eshakar <102459513+eshakar@users.noreply.github.com>
2024-05-05 20:36:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+() => {
+  cy.get('button').first().click()
+    .then(() => {
+      cy.get('.modal').should('be.visible');
+      cy.get('.close').click();
+    })
+    .then(() => cy.get('.modal').should('not.be.visible'));
+};
