19d52311aec75cf0cec2245a07f0cb5c42fdf4d4
eshakar <102459513+eshakar@users.noreply.github.com>
2024-05-05 20:44:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
 //your JS code here. If required.
- document.getElementById('open-modal').addEventListener('click', () => {
-            document.getElementById('modal').classList.remove('hidden');
-        });
-
-        document.getElementById('close-modal').addEventListener('click', () => {
-            document.getElementById('modal').classList.add('hidden');
-        });
