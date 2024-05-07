39e6819a4487e793d088fa734f5ed880b60c3481
eshakar <102459513+eshakar@users.noreply.github.com>
2024-05-07 21:02:32 +0530

    created a submission

--- script.js
+++
@@ -1,9 +0,0 @@
-//your JS code here. If required.
-it('testing modal close functionality', () => {
-  cy.get('button').first().click().then(() => {
-    cy.get('.modal').should('be.visible');
-    // Use contains selector
-    cy.get('.modal-header button').contains('close').click();
-    cy.get('.modal').should('not.be.visible');
-  });
-});
